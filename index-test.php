f1324fdd2e25d5b107dea9da870c52c93352fcef
Terence Monteiro <terence@rmcbangalore.com>
2013-06-08 18:37:59 +0530

    yii framework path in version specific subfolder, assets folders deleted

--- index-test.php
+++ index-test.php
@@ -5,7 +5,7 @@
  */
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/../../yii/yii-1.1.13.e9e4a0/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/test.php';
 
 // remove the following line when in production mode
--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../yii/framework/yiit.php';
+$yiit=dirname(__FILE__).'/../../../../yii/yii-1.1.13.e9e4a0/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 require_once($yiit);
