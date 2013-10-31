2369c71027725f2f2d772fbe519f3fdd94ccd6cf
Terence Monteiro <terence@rmcbangalore.com>
2013-10-31 11:40:52 +0530

    Deleting InstallController not automatic, showing a message to do this

--- protected/views/install/success.php
+++ protected/views/install/success.php
@@ -8,15 +8,14 @@ $this->breadcrumbs=array(
 $dir = dirname(__FILE__);
 $inst_path = preg_replace('?views/install?', 'controllers/InstallController.php', $dir);
 
-if (is_writable($inst_path)) {
-	unlink($inst_path);
-}
+# we will manually delete this install controller file. later plan to do this on first login
 
 ?>
 <h1>Congrats! Installation Complete!!</h1>
 
 <p>
 You have successfully installed St. Bennos Parish Software!!<br />
+You may now delete the file <?php echo $inst_path ?>.
 Kindly click the following link to login: <a href="<?php echo Yii::app()->createUrl('/site/login') ?>">Login</a>
 </p>
 
