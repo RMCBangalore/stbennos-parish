be54564363ac5e5e79789cd7b47d7977604361de
Terence Monteiro <terence@rmcbangalore.com>
2013-11-07 02:29:09 +0530

    Added README and licensing information

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,4 +1,25 @@
-<?php /* @var $this Controller */ ?>
+<?php
+#
+# This file is part of St. Benno's Parish Software
+#
+# St. Benno's Parish Software - software to manage tomorrow's parish
+# Copyright (C) 2013  Redemptorist Media Center
+#
+# St. Benno's Parish Software is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# St. Benno's Parish Software is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
