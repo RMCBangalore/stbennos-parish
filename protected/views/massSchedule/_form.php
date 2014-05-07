a03a71711b302d6f6fb1a0f4dd9e7bd357eb209f
Terence Monteiro <terence@rmcbangalore.com>
2014-05-07 14:47:36 +0530

    Reformatted Add Mass ajax form with fieldset, etc

--- protected/views/massSchedule/_form.php
+++ protected/views/massSchedule/_form.php
@@ -30,26 +30,26 @@
 	'enableAjaxValidation'=>false,
 )); ?>
 
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
+	<!--p class="note">Fields with <span class="required">*</span> are required.</p-->
 
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'time'); ?>
-		<?php echo $form->textField($model,'time',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'time'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'language'); ?>
-		<?php echo $form->dropDownList($model,"language",FieldNames::values('languages')); ?>
-		<?php echo $form->error($model,'language'); ?>
-	</div>
-
-	<div class="row">
+	<span class="leftThird">
 		<?php echo $form->labelEx($model,'day'); ?>
 		<?php echo $form->dropDownList($model,"day",FieldNames::values('weekdays')); ?>
 		<?php echo $form->error($model,'day'); ?>
+	</span>
+	<span class="centerThird">
+		<?php echo $form->labelEx($model,'time'); ?>
+		<?php echo $form->textField($model,'time',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->error($model,'time'); ?>
+	</span>
+	<span class="rightThird">
+		<?php echo $form->labelEx($model,'language'); ?>
+		<?php echo $form->dropDownList($model,"language",FieldNames::values('languages')); ?>
+		<?php echo $form->error($model,'language'); ?>
+	</span>
 	</div>
 
 	<div class="row buttons">
--- protected/views/massSchedule/create-ajax.php
+++ protected/views/massSchedule/create-ajax.php
@@ -1,1 +1,34 @@
+<style>
+fieldset {
+	border: 1px solid #aaa;
+	padding: 10px 20px;
+}
+fieldset legend {
+	font-weight: bold;
+	padding: 1px 4px;
+}
+#mass-schedule-form {
+	width: 400px;
+}
+#mass-schedule-form span.leftThird,
+#mass-schedule-form span.centerThird,
+#mass-schedule-form span.rightThird {
+	width: 32%;
+}
+div.row {
+	text-align: center;
+}
+div.row.buttons {
+	width: 98%;
+}
+div.row.buttons input {
+	width: 270px;
+	padding: 3px 10px;
+	border-width: 2px;
+}
+</style>
+
+<fieldset>
+<legend>Add Mass</legend>
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</fieldset>
